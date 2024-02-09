df4f794c417db19755ef42f770a57b8491ab6370
chiyo <58620794+S-chiyo@users.noreply.github.com>
2024-02-09 19:15:25 +0900

    Add files via upload

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+server_address = '/socket_file'
+print('connecting to {}'.format(server_address))
+
+try:
+    sock.connect(server_address)
+except socket.error as err:
+    print(err)
+    sys.exit(1)
+
+input_message = input('please write your message: ')
+
+try:
+    message = input_message.encode()
+    sock.sendall(message)
+    sock.settimeout(2)
+
+    try:
+        while True:
+            data = sock.recv(32)
+            if data:
+                print(data.decode('utf-8'))
+            else:
+                break
+    except(TimeoutError):
+        print('Socket timeout, ending listening for server messages')
+
+finally:
+    print('closing socket')
+    sock.close()
